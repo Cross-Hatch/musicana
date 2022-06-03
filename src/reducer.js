b4336974fe95932f93a2a422cdbc727f40e31f02
GideonAg <gideonagbosu72@gmail.com>
2022-06-03 02:50:41 +0000

    Modified reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -5,7 +5,7 @@ export const initialState = {
     item: null,
 
     //  REMOVE THE TOKEN VALUE AFTER FINISHING
-    token: 'BQAvnTgJCsFtsWIQDi0ZK5Lz73-wg3Mh_CGLIYPbMiLheZYaL4iiwi4llT0G7IKtfFmBfZin0nhCnOf2iRcUOJOgaGmOj5v4aP9PMDmcHqewt44G_X2nDm1xg9IQlZzrpHweqJ-0byenj08vlZz3NqfamotD19wZo6RI2FcXcqTuY1brt1Ky',
+    // token: 'BQAvnTgJCsFtsWIQDi0ZK5Lz73-wg3Mh_CGLIYPbMiLheZYaL4iiwi4llT0G7IKtfFmBfZin0nhCnOf2iRcUOJOgaGmOj5v4aP9PMDmcHqewt44G_X2nDm1xg9IQlZzrpHweqJ-0byenj08vlZz3NqfamotD19wZo6RI2FcXcqTuY1brt1Ky',
 };
 
 const reducer = (state, action) => {
@@ -15,14 +15,21 @@ const reducer = (state, action) => {
         case 'SET_USER':
             return {
                 ...state,
-                user: action.user
+                user: action.user,
             };
         
         case 'SET_TOKEN':
             return{
                 ...state,
-                token: action.token
+                token: action.token,
             }
+        
+        case 'SET_PLAYLIST':
+            return{
+                ...state,
+                playing: action.playlist,
+            }
+            
         default:
             return state;
     }
