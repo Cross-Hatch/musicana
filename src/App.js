43547e7a0d666fac4e95d06aba38ae2659472ccc
GideonAg <gideonagbosu72@gmail.com>
2022-06-04 23:09:14 +0000

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Login from './components/Login/Login';
-import { getTokenFromUrl } from './components/Login/spotify';
-import SpotifyWebApi from 'spotify-web-api-js';
-import Player from './components/Player/Player';
-import { useDataLayerValue } from './DataLayer';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Login from "./components/Login/Login";
+import { getTokenFromUrl } from "./components/Login/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./components/Player/Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [{ user, token }, dispatch] = useDataLayerValue();
 
-  useEffect (() =>{
+  useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
 
-    if(_token){
-
+    if (_token) {
       dispatch({
-        type: 'SET_TOKEN',
+        type: "SET_TOKEN",
         token: _token,
       });
 
       spotify.setAccessToken(_token);
 
-      spotify.getMe().then(user => {
+      spotify.getMe().then((user) => {
         dispatch({
-          type: 'SET_USER',
+          type: "SET_USER",
           user: user,
         });
       });
 
       spotify.getUserPlaylists().then((playlist) => {
         dispatch({
-          type: 'SET_PLAYLIST',
+          type: "SET_PLAYLIST",
           playlist: playlist,
         });
       });
+
+      spotify.getPlaylist("74k1p1IocpWIcwQs1g5R1L").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
     }
   }, []);
 
   return (
     <div className="App">
-      {
-        token ? <Player spotify={spotify} /> : <Login />
-      }
+      {token ? <Player spotify={spotify} /> : <Login />}
     </div>
   );
 }
