0bef99cbab8752d2d436b986e937c70c18da15b1
GideonAg <gideonagbosu72@gmail.com>
2022-06-04 12:19:12 +0000

    Modified Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,51 @@
-import './Footer.css';
+import "./Footer.css";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import { Grid, Slider } from "@mui/material";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
 
 function Footer() {
   return (
-    <div className='footer'>
-        I'm the footer
+    <div className="footer">
+      <div className="footer_left">
+        <img
+          className="footer_albumLogo"
+          src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+          alt=""
+        ></img>
+        <div className="footer_songInfo">
+          <h4>Hey ya ya!</h4>
+          <p>Victoria Orenzi</p>
+        </div>
+      </div>
+
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon className="footer_icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer_icon" />
+        <SkipNextIcon className="footer_icon" />
+        <RepeatIcon className="footer_green" />
+      </div>
+
+      <div className="footer_right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
